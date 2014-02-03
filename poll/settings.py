6b544cd4ddb884835db7bb710f909171be54a608
manjunath24 <hugar.manjunathrt@gmail.com>
2014-02-03 16:22:10 +0530

    settings for heroku

--- poll/settings.py
+++ poll/settings.py
@@ -91,10 +91,11 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+if 'herokupoll' in os.environ:
+	import dj_database_url
+	DATABASES['default'] =  dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+	# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
